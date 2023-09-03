3345b1cd31fc16c88f5f1a76a24998e458f8e36e
Magdiel Castillo <magdielmagdiel01@gmail.com>
2023-09-03 18:59:19 -0400

    urls cleaning

--- index.php
+++ index.php
@@ -16,26 +16,7 @@ if($param == "") {
     root. "salir" => "controllers/logout.controller.php",
     root. "alumnos" => "controllers/students.controller.php",
     root. "cursos" => "controllers/courses.controller.php",
-    root. "create" => "controllers/create.controller.php"/*,
-    root. "custom-exclusive" => "controllers/custom-recipe-exclusive.controller.php",
-    root. "profile" => "controllers/profile.controller.php",
-    root. "ingredients" => "controllers/ingredients.controller.php",
-    root. "add-recipe" => "controllers/add-recipe.controller.php",
-    root. "categories" => "controllers/categories.controller.php",
-    root. "user" => "controllers/users.controller.php", 
-    root. "email" => "controllers/email.controller.php",
-    root. "signup" => "controllers/signup.controller.php",
-    root. "recovery" => "controllers/recovery.controller.php",
-    root. "error404" => "controllers/404.controller.php",
-    root. "terms-and-conditions" => "controllers/terms.controller.php",   
-    root. "not-found" => "controllers/notfound.controller.php",
-    root. "notifications" => "controllers/notification.controller.php",
-    root. "recycle" => "controllers/recycle.controller.php",
-    root. "reactivate-account" => "controllers/reactivate-account.controller.php",
-    root. "reactivate" => "controllers/reactivate.controller.php",
-    root. "settings" => "controllers/settings.controller.php",
-    root. "update" => "controllers/update.controller.php",
-    root. "diet" => "controllers/diet.controller.php"  */
+    root. "create" => "controllers/create.controller.php"
     ];
 //Si hay parámetros
 } else {
